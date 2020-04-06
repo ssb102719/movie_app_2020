5401d1710f0c68af62a284ae3002a1df7f1efbb8
unknown <ssb102719@naver.com>
2020-04-06 20:23:08 +0900

    Updates

---
+++ precache-manifest.e8db3364c2219fc395242a4d7bb191ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c559e07f36331119c58d33d1400cc7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4739ba3e9eaa5fda834e",
+    "url": "/movie_app_2020/static/css/main.3f07dd79.chunk.css"
+  },
+  {
+    "revision": "2731f645873efd1058d0",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4739ba3e9eaa5fda834e",
+    "url": "/movie_app_2020/static/js/main.08903729.chunk.js"
+  },
+  {
+    "revision": "8b67d7ccc50a17c9e597",
+    "url": "/movie_app_2020/static/js/runtime-main.aaeb0f22.js"
+  }
+]);
